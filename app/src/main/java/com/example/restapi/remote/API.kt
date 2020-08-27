1c983278388abbe64df8f7906577e05baf21109a
Cris <cristian.vidal.lopezgmail.com>
2020-08-27 13:03:58 -0400

    Lo visto en clases

---
+++ app/src/main/java/com/example/restapi/remote/API.kt
@@ -0,0 +1,20 @@
+package com.example.restapi.remote
+
+import com.example.restapi.pojo.Post
+import retrofit2.Call
+import retrofit2.http.*
+import java.util.*
+
+interface Api {
+    @GET("/posts")
+    fun getAllPosts(): Call<ArrayList<Post>>
+
+    @DELETE("/posts/{postId}")
+    fun deletePost(@Path("postId") postId: Int?) : Call<Void>
+
+   // @Headers("Content-Type: application/json; charset=UTF-8")
+    @POST("/posts")
+    fun createNewPost(@Body post: Post): Call<Post>
+
+
+}
